1d438893095e4e47b77e5eefa25e09743e6c82a4
Benchocpa <152940706+Benchocpa@users.noreply.github.com>
2025-11-09 14:10:12 -0500

    Update Firebase configuration details

--- src/firebase.js
+++ src/firebase.js
@@ -3,10 +3,10 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBFps3GRnlyumFb9d1gTX-YqCePgNaxa3s",
+  apiKey: "AIzaSyBFps3GRnLyumFb9d1gTX-YqCePgNaxa3s",
   authDomain: "bitacora-ef9b5.firebaseapp.com",
   projectId: "bitacora-ef9b5",
-  storageBucket: "bitacora-ef9b5.firebasestorage.app",
+  storageBucket: "bitacora-ef9b5.appspot.com", // 👈 CAMBIO AQUÍ
   messagingSenderId: "313158303573",
   appId: "1:313158303573:web:748f5f7827db70bd5d39dc",
   measurementId: "G-HL3KCZPGCN"
@@ -21,3 +21,4 @@ export const db = getFirestore(app);
 // Mensaje de prueba en consola
 console.log("🔥 Firestore conectado:", db);
 console.log("Proyecto:", app.options.projectId);
+
